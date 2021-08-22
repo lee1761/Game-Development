9be986f208d3fb210e4ccd907e3f913f7930bc8c
lee1761 <premierdkel@gmail.com>
2021-08-22 17:00:55 +0930

    a
    aa

---
+++ New Unity Project/Assets/2Scripts/Instruction/Dialogue.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+
+public class Dialogue : MonoBehaviour
+{
+	public TextMeshProUGUI textComponent;
+
+	[TextArea(10,10)]
+	public string[] lines;
+	public float textSpeed;
+    
+    private int index;
+
+
+
+	public void Click()
+    {
+    	SceneManager.LoadScene(2);
+        
+    }
+    void StartDialogue()
+    {
+        index =0;
+        StartCoroutine(TypeLine());
+
+    }
+
+   
+   void Start()
+    {
+        textComponent.text = "   ";
+        StartDialogue();    
+    }
+
+    public void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            {
+        if(textComponent.text == lines[index])
+        {
+            NextLine();
+        }
+        else{
+            StopAllCoroutines();
+            textComponent.text = lines[index];
+        }
+    }
+
+        }
+    
+
+    
+    IEnumerator TypeLine()
+    {
+    	foreach (char c in lines[index].ToCharArray())
+    	{
+    		textComponent.text += c;
+    		yield return new WaitForSeconds(textSpeed);
+    	}
+    }
+    
+     void NextLine()
+    {
+        
+        if(index < lines.Length - 1)
+        {
+            index ++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else{
+            //gameObject.SetActive(false);
+        }
+    
+    }
+
+
+
+}
+
---
+++ New Unity Project/Assets/2Scripts/KoreaZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class KoreaZone : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        SceneManager.LoadScene(5);
+    }
+
+    public void Click()
+    {
+        SceneManager.LoadScene(5);
+    }
+}
